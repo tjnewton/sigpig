947d1cd4c1b0f2d85d7a2442d6f7b0d9dd499277
Tyler Newton <tjnewton.uni@gmail.com>
2021-08-07 11:39:57 -0700

    add example for station locations function

--- sigpig/core/data.py
+++ sigpig/core/data.py
@@ -553,7 +553,20 @@ def trim_Daily_Waveforms(project_Name: str, start_Time, end_Time, channels:
 
 def rattlesnake_Ridge_Station_Locations():
     """ Returns a dict of station locations, used by EQTransformer
-    downloader.stationListFromMseed to create a station_list.json file"""
+    downloader.stationListFromMseed to create a station_list.json file
+
+    Example: write station locations to a file
+        station_locations = rattlesnake_Ridge_Station_Locations()
+
+        # now write to file
+        with open("station.locs", "w") as file:
+            for station in station_locations.keys():
+                latitude = station_locations[station][0]
+                longitude = station_locations[station][1]
+                line = f"{station},{latitude},{longitude}"
+                file.write(line)
+
+    """
     stations = (1,2,3,4,5,6,7,8,9,10,12,13,15,16,17,18,20,21,22,23,25,26,27,
                 28,30,31,32,33,34,35,36,37,38,39,40,41,42,'UGAP3','UGAP5',
                 'UGAP6')
