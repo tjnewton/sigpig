d0ee9909395355d42c86517c7c948f9e511d800f
Tyler Newton <tjnewton.uni@gmail.com>
2021-08-10 12:13:38 -0700

    fix short circuit in stack_waveforms_1x1

--- sigpig/core/lfe_finder.py
+++ sigpig/core/lfe_finder.py
@@ -872,8 +872,6 @@ def stack_waveforms_1x1(party, pick_offset, streams_path, template_length,
                 # and generate linear stack
                 stack_lin += linstack(streams=group_streams)
 
-                return [stack_pw, stack_lin]
-
             except Exception:
                 pass
 
