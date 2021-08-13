178844c38e63019a1a2290d09afd834d60d1e318
Tyler Newton <tjnewton.uni@gmail.com>
2021-08-13 11:11:30 -0700

    fix stack_Waveforms example

--- sigpig/core/lfe_finder.py
+++ sigpig/core/lfe_finder.py
@@ -1077,7 +1077,7 @@ def stack_waveforms_alt(party, pick_offset, streams_path, template_length,
             # get group streams to stack (only a single group here)
             group_streams = [st_tuple[0] for st_tuple in stream_list]
 
-            # loop over each detection in group
+            # loop over each detection in group and time shift
             for group_idx, group_stream in enumerate(group_streams):
                 # align traces before stacking
                 for trace_idx, trace in enumerate(group_stream):
