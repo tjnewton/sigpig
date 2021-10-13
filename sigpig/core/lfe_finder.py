6964ab27da1547daeed5cb65f6068c5d05c56284
Tyler Newton <tjnewton.uni@gmail.com>
2021-10-13 16:06:53 -0700

    update comments with latest run, different template

--- sigpig/core/lfe_finder.py
+++ sigpig/core/lfe_finder.py
@@ -1186,8 +1186,8 @@ def detect_signals(templates, template_files, station_dict, template_length,
 
         try:
             # detect
-            party = tribe.detect(stream=st, threshold=0.25, daylong=True,
-                                 threshold_type="abs", trig_int=8.0,
+            party = tribe.detect(stream=st, threshold=10.0, daylong=True,
+                                 threshold_type="MAD", trig_int=8.0,
                                  plot=False,
                                  return_stream=False, parallel_process=False,
                                  ignore_bad_data=True)
@@ -2250,7 +2250,7 @@ def find_LFEs(templates, template_files, station_dict, template_length,
         # end_date = UTCDateTime("2018-08-11T23:59:59.9999999999999Z")
 
         # set snr threshold to cull the party detections
-        snr_threshold = 3.5 #1.0
+        snr_threshold = 1.0 # 3.5
 
         # define the main trace to use for detections (best amplitude station)
         main_trace = ("TA", "N25K", "BHN")
@@ -2305,8 +2305,14 @@ def find_LFEs(templates, template_files, station_dict, template_length,
         ###########################  TEMPLATE 2  ##############################
         ###################### 2016  9 27  7 37 49.00 #########################
         #######################################################################
-        # abs 0.25 =  detections
+        # abs 0.25 = 289 detections
         infile = open('party_06_15_2016_to_08_12_2018_t2_abs.25_16s.pkl', 'rb')
+
+        # MAD 10.0 = 460 detections
+        # infile = open('party_06_15_2016_to_08_12_2018_t2_MAD10_16s.pkl','rb')
+
+        # MAD 9.0 =  detections
+        # infile = open('party_06_15_2016_to_08_12_2018_t2_MAD9_16s.pkl','rb')
 
         # MAD 8.0 = 3388 detections
         # infile = open('party_06_15_2016_to_08_12_2018_t2_MAD8_16s.pkl', 'rb')
@@ -2357,7 +2363,7 @@ def find_LFEs(templates, template_files, station_dict, template_length,
                                                align_type=shift_method)
         # save stacks as pickle file
         # abs 0.25: h m to stack
-        outfile = open(f'inner_stack_0_snr{snr_threshold}_'
+        outfile = open(f'inner_stack_t2_0_snr{snr_threshold}_'
                        f'{shift_method}Shift_abs.25_16s.pkl', 'wb')
         # MAD 8: 6h 35m to stack
         # outfile = open(f'inner_stack_0_snr{snr_threshold}_'
@@ -2370,12 +2376,12 @@ def find_LFEs(templates, template_files, station_dict, template_length,
 
     if plot:
         if len(stack_pw) > 0:
-            plot_stack(stack_pw, title=f'phase_weighted_stack_snr'
+            plot_stack(stack_pw, title=f'phase_weighted_stack_t2_snr'
                        f'{snr_threshold}_{shift_method}'
-                       f'Shift_abs.25_16s', save=True)
+                       f'Shift_MAD10_16s', save=True)
         if len(stack_lin) > 0:
-            plot_stack(stack_lin, title=f'linear_stack_snr{snr_threshold}_'
-                       f'{shift_method}Shift_abs.25_16s',
+            plot_stack(stack_lin, title=f'linear_stack_t2_snr{snr_threshold}_'
+                       f'{shift_method}Shift_MAD10_16s',
                        save=True)
 
             # now plot template with the linear stack from same station for
