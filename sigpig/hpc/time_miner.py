b992eba582c416f3ad963ff8260a3b426470db75
Tyler Newton <tjnewton.uni@gmail.com>
2021-10-14 12:52:05 -0700

    delete fixed FIXME

--- sigpig/hpc/time_miner.py
+++ sigpig/hpc/time_miner.py
@@ -372,7 +372,6 @@ def find_Picking_Windows(filepaths: list, detection_Parameters:
 def rattlesnake_Ridge_Station_Locations():
     """ Returns a dict of station locations, used by EQTransformer
     downloader.stationListFromMseed to create a station_list.json file"""
-    # FIXME: need to add locs for 14, 24
     stations = (1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,20,21,22,23,24,25,
                 26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,'UGAP3',
                 'UGAP5','UGAP6')
