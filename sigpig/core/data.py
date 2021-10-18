7b5d5d4e555b0fd43b74ad7e86ae9cc18d191990
Tyler Newton <tjnewton.uni@gmail.com>
2021-10-18 13:41:27 -0700

    add time dependent station implementation to RR locations function

--- sigpig/core/data.py
+++ sigpig/core/data.py
@@ -701,8 +701,32 @@ def rattlesnake_Ridge_Station_Locations(date, format=None):
     #      : and equivalent for UGAP
     stations = project_stations("Rattlesnake Ridge", date)
 
-    # get time dependent locations
-
+    # get time dependent locations. note: this is not robust so doing
+    # analyses around the edges of deployments is a bad idea
+    if date <= UTCDateTime("2018-04-08T22:00:00.0Z"):
+        stas = [1, 2, 3, 5, 4, 6, 7, 8, 13, 9, 10, 12, 15, 'UGAP3', 16, 17,
+                18, 20, 21, 22, 23, 25, 26, 27, 'UGAP5', 'UGAP6', 28, 30,
+                31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
+    elif date <= datetime(2018, 5, 7):
+        stas = [1, 2, 3, 5, 4, 6, 7, 8, 13, 9, 10, 12, 14, 15, 'UGAP3', 16,
+                17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 'UGAP5', 'UGAP6',
+                28, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
+    elif date == datetime(2018, 5, 8):
+        stas = [1, 3, 5, 4, 6, 7, 8, 13, 9, 10, 12, 14, 15, 'UGAP3', 16,
+                17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 'UGAP5', 'UGAP6',
+                28, 29, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
+    elif date <= datetime(2018, 6, 6):
+        stas = [1, 3, 5, 4, 6, 7, 8, 13, 9, 10, 12, 14, 15, 'UGAP3', 16,
+                17, 18, 20, 21, 22, 23, 24, 25, 26, 27, 'UGAP5', 'UGAP6',
+                28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
+    elif date == datetime(2018, 6, 7):
+        stas = [1, 3, 5, 4, 6, 7, 8, 13, 9, 10, 14, 15, 'UGAP3', 16, 18,
+                20, 21, 22, 23, 24, 25, 26, 27, 'UGAP5', 'UGAP6', 28, 29,
+                30, 31, 32, 33, 34, 35, 36, 37, 38, 40, 41, 42]
+    else:
+        stas = [1, 2, 3, 5, 4, 6, 7, 8, 13, 9, 10, 14, 15, 'UGAP3', 16, 18,
+                20, 21, 22, 23, 24, 25, 26, 27, 'UGAP5', 'UGAP6', 28, 29,
+                30, 31, 32, 33, 34, 35, 36, 37, 38, 40, 41, 42]
 
     # in decimal degrees format
     stations = (1,2,3,4,5,6,7,8,9,10,12,13,15,16,17,18,20,21,22,23,25,26,27,
