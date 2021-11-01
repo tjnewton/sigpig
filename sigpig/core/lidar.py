0929646de000f7cfbd5b092776c3a3daf95c80be
Tyler Newton <tjnewton.uni@gmail.com>
2021-11-01 14:05:23 -0700

    build grids_from_raster

--- sigpig/core/lidar.py
+++ sigpig/core/lidar.py
@@ -347,7 +347,7 @@ def grids_from_raster(raster_file, x_limits, y_limits, xy_grid_nodes):
         raster_file = '/Users/human/Dropbox/Programs/lidar/yakima_basin_2018_dtm_43.tif'
         x_limits = [-120.480, -120.462]
         y_limits = [46.519, 46.538]
-        xy_grid_nodes = [100, 100]
+        xy_grid_nodes = [1000, 1000]
 
         # query raster at specified coordinates
         longitude_grid, latitude_grid, elevation_grid = grids_from_raster(raster_file, x_limits, y_limits, xy_grid_nodes)
@@ -378,15 +378,23 @@ def grids_from_raster(raster_file, x_limits, y_limits, xy_grid_nodes):
         latitude_grid.append(row_latitudes)
         elevation_grid.append(row_elevations)
 
+    # save it?
+
+    longitude_grid = np.asarray(longitude_grid)
+    latitude_grid = np.asarray(latitude_grid)
+    elevation_grid = np.asarray(elevation_grid)
+
     import matplotlib as mpl
     mpl.use('macosx')
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    surf = ax.plot_surface(a, b, c, cmap=cm.coolwarm, vmin=np.nanmin(c),
-                           vmax=np.nanmax(c), linewidth=0, antialiased=False)
-    ax.set_zlim(np.nanmin(c), np.nanmax(c) + 1000)
+    surf = ax.plot_surface(longitude_grid, latitude_grid, elevation_grid,
+                           cmap=cm.coolwarm,
+                           vmin=np.nanmin(elevation_grid),
+                           vmax=np.nanmax(elevation_grid), linewidth=0, antialiased=False)
+    ax.set_zlim(np.nanmin(elevation_grid), np.nanmax(elevation_grid) + 1000)
     ax.xaxis.set_major_locator(LinearLocator(10))
     ax.yaxis.set_major_locator(LinearLocator(10))
-    plt.xlim(max(x), min(x))
+    plt.xlim(max(longitude_grid), min(longitude_grid))
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
 
