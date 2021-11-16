f028ce83123278ea8a4396263496c590c2f8305a
Tyler Newton <tjnewton.uni@gmail.com>
2021-11-16 15:14:09 -0800

    update station location file path

--- sigpig/core/stingray_ttg_2_nonlinloc.m
+++ sigpig/core/stingray_ttg_2_nonlinloc.m
@@ -1,7 +1,7 @@
 clear all;
 
 % load station locations
-load /Users/human/Dropbox/Research/Rattlesnake_Ridge/rattlesnake/stalocs_1.dat
+load /Users/human/Dropbox/Programs/stingray/projects/rattlesnake_ridge/station_locations.dat
 
 % loop over each station's srRays file
 for n =[10 12 13 1 15 16 17 18 2 20 21 22 23 25 26 27 28 3 30 31 32 33 34 35 36 37 38 39 4 40 41 42 5 6 7 8 9 103 105 106]
@@ -27,12 +27,12 @@ for n =[10 12 13 1 15 16 17 18 2 20 21 22 23 25 26 27 28 3 30 31 32 33 34 35 36 
     end
     fclose(file_id)
 
-    ind=find(stalocs_1(:,1)==n)
+    ind=find(station_locations(:,1)==n)
 
     % build .hdr file
     file_id = fopen(['RR.P.' num2str(n) '.time.hdr'],'w');
     fprintf(file_id,'%d %d %d %f %f %f %f %f %f %s\n', srRays.nx, srRays.ny, srRays.nz, ...
         srRays.srGeometry.easting, srRays.srGeometry.northing, 0, srRays.gx, srRays.gy, srRays.gz, 'TIME');
-    fprintf(file_id,'%s %f %f %f\n', num2str(n), stalocs_1(ind,4)/1000, stalocs_1(ind,5)/1000, 0); %-1*stalocs_1(ind,6)/1000);
+    fprintf(file_id,'%s %f %f %f\n', num2str(n), station_locations(ind,2), station_locations(ind,3), -1*station_locations(ind,4)); % or 0);
     fclose(file_id);
 end
