fd06cafb7dba37c8f335fd09e19f95bd9cdb2f8c
Tyler Newton <tjnewton.uni@gmail.com>
2021-12-03 19:42:24 -0800

    update station file path

--- sigpig/core/stingray_ttg_2_nonlinloc.m
+++ sigpig/core/stingray_ttg_2_nonlinloc.m
@@ -1,7 +1,7 @@
 clear all;
 
 % load station locations
-load /Users/human/Dropbox/Programs/stingray/projects/rattlesnake_ridge/station_locations.dat
+load /Users/human/Dropbox/Research/Rattlesnake_Ridge/stingray_rr/station_locations.dat
 
 % loop over each station's srRays file
 for n =[10 12 13 1 15 16 17 18 2 20 21 22 23 25 26 27 28 3 30 31 32 33 34 35 36 37 38 39 4 40 41 42 5 6 7 8 9 103 105 106]
