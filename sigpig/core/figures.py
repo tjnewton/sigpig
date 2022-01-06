8d53b4fcfd39cc1f1ff38a647a279a2e903eaa84
Tyler Newton <tjnewton.uni@gmail.com>
2022-01-05 21:19:10 -0800

    delete old function shell

--- sigpig/core/figures.py
+++ sigpig/core/figures.py
@@ -1067,11 +1067,6 @@ def plot_distribution(data, bins=False, title=False, save=False):
     return fig
 
 
-def nll_locs_to_():
-
-    pass
-
-
 def examine_stack():
     "Animate building of stack"
 
