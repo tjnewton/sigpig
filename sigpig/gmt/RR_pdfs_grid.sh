49edadf4594067fe78f4ae410d7209692323de79
Tyler Newton <tjnewton.uni@gmail.com>
2022-01-17 20:45:51 -0800

    add yz pdf grid to figure

--- sigpig/gmt/RR_pdfs_grid.sh
+++ sigpig/gmt/RR_pdfs_grid.sh
@@ -11,9 +11,9 @@ VEL_RANGE=0.6-0.75
 gmt begin pdfs_$VEL_RANGE png
 
 	# first subplot
-	gmt subplot begin 2x1 -Fs15c/25c,5c -A
+	gmt subplot begin 2x2 -Fs15c,7c/25c,5c -A
 	# generate a basemap for the region
-	gmt basemap -Rk694.150000/694.448000/5155.400000/5155.898000 -JU+10/? -Baf -BWeNs+t"Event PDF Locations Vp=$VEL_RANGE"
+	gmt basemap -Rk694.150000/694.448000/5155.400000/5155.898000 -JU+10/? -Baf -BWeNs+t"Event PDF Locations Vp=$VEL_RANGE (SSST)"
 
 	# # set lighting options for hillshade
 	gmt grdgradient gridded_rr_dtm.nc -A310 -Nt0.6 -Grelief_gradient.nc # -A320
@@ -28,7 +28,7 @@ gmt begin pdfs_$VEL_RANGE png
 	gmt grdimage gridded_rr_dtm.nc -C -Irelief_gradient.nc -t15 # -I+a320+nt0.6+m0 -Cdem4,
 
 	# trim and plot the colorbar
-	gmt colorbar -DJMR+o0.8c/-6+w10c/0.3c -B50 -Bx+l"Elevation (m)" -G290/520
+	gmt colorbar -DJMR+o11.0c/-6+w10c/0.3c -B50 -Bx+l"Elevation (m)" -G290/520
 
 	# get sums of pdf weights (-Ss), on 1 m grid in x and y (-I1e/1e)
 	# gmt blockmean xyw_$VEL_RANGE.csv -I10e/10e -Ss >> counts_$VEL_RANGE.xyz
@@ -42,7 +42,7 @@ gmt begin pdfs_$VEL_RANGE png
 	# plot the grid
 	gmt grdview gridded_rr_pdfs.nc -C -Qs -t40 #m
 	# trim and plot the colorbar
-	gmt colorbar -C -DJMR+o0.8c/6+w10c/0.3c -B1000 -Bx+l"PDF sum" # -G0/50000
+	gmt colorbar -C -DJMR+o11.0c/6+w10c/0.3c -B1000 -Bx+l"PDF sum" # -G0/50000
 
 
 	# plot stations at triangles
@@ -51,11 +51,21 @@ gmt begin pdfs_$VEL_RANGE png
 	# build subplot with depth & longitude of locations
 	gmt subplot set 1,0 -Cn-1.3
 		# make a custom colormap from a built-in colormap
-		gmt makecpt -Cabyss -T0/10000  # -H >> pdfs.cpt
+		gmt makecpt -Cabyss -T0/10000
 		# plot the grid
 		gmt grdview gridded_rr_pdfs_xz.nc -C -Qs -t40 -R-120.4683/-120.4646/-150/5 -JX? -Bx+l"Longitude" -By+l"Depth (m)" -BWrtS -W0.1,black # -Baf  
 		# trim and plot the colorbar
-		gmt colorbar -C -DJMR+o0.8c/0.5+w5c/0.3c -B1000 -Bx+l"PDF sum" # -G0/50000
+		gmt colorbar -C -DJMR+o0.8c/0.1+w5c/0.3c -B1000 -Bx+l"PDF sum" # -G0/50000
+	# gmt subplot end
+
+	# build subplot with depth & latitude of locations
+	gmt subplot set 0,1 -Cw-1.0
+		# make a custom colormap from a built-in colormap
+		gmt makecpt -Cabyss -T0/10000
+		# plot the grid
+		gmt grdview gridded_rr_pdfs_yz.nc -C -Qs -t40 -R-5/150/46.52415/46.52843 -JX7c/25c -Bx+l"Depth(m)" -By+l"Latitude" -BEwtS -W0.1,black # -Baf  
+		# trim and plot the colorbar
+		# gmt colorbar -C -DJMR+o0.8c/6+w10c/0.3c -B1000 -Bx+l"PDF sum" # -G0/50000
 	gmt subplot end
 
 gmt end show
