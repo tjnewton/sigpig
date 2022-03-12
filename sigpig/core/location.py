17c2a415824e73ef0a7f9e4b3b6e91cc9559296e
Tyler Newton <tjnewton.uni@gmail.com>
2022-03-11 16:44:11 -0800

    add masked velocity model path to location_pdfs_to_grid

--- sigpig/core/location.py
+++ sigpig/core/location.py
@@ -633,7 +633,8 @@ def location_pdfs_to_grid(pdfs, project_name):
             # file_path = "/Users/human/Dropbox/Research/Rattlesnake_Ridge/nlloc_ssst-coh_rr_0.4-0.55_newPicks/relocated/RR.hyp"
             # file_path = "/Users/human/Dropbox/Research/Rattlesnake_Ridge/nlloc_ssst-coh_rr_0.6-0.75_214Picks/relocated/RR.hyp"
             # file_path = "/Users/human/Dropbox/Research/Rattlesnake_Ridge/nlloc_ssst-coh_rr_0.5-0.65_214Picks/relocated/RR.hyp"
-            file_path = "/Users/human/Dropbox/Research/Rattlesnake_Ridge/nlloc_ssst-coh_rr_0.4-0.55_214Picks/relocated/RR.hyp"
+            # file_path = "/Users/human/Dropbox/Research/Rattlesnake_Ridge/nlloc_ssst-coh_rr_0.4-0.55_214Picks/relocated/RR.hyp"
+            file_path = "/Users/human/Dropbox/Research/Rattlesnake_Ridge/nlloc_ssst-coh_rr_0.6-0.75_214Picks_OSOS/relocated/RR.hyp"
             pdfs, hypocenters = extract_nll_locations(file_path)
             project_name = "Rattlesnake Ridge"
             location_pdfs_to_grid(pdfs, project_name)
