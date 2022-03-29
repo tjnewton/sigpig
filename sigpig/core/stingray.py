29843ed3a43a5aa6d0251eb072b30ed8c1c1a15b
Tyler Newton <tjnewton.uni@gmail.com>
2022-03-28 21:40:33 -0700

    add 0.75 velocity RR runs

--- sigpig/core/stingray.py
+++ sigpig/core/stingray.py
@@ -232,7 +232,7 @@ def stingray_setup(project_name: str, date: UTCDateTime):
             # velmod = pd.read_csv("/Users/human/git/sigpig/sigpig/stingray/m-files/vels.txt", delim_whitespace=True)
 
             # define a slowness factor to multipy onslide slowness by
-            slowness_factor = 2
+            slowness_factor = 3/4 # 2 for half-speed,
 
             # original velocity model based on Vs30
             velocity_model = [[0.00, 0.60],
@@ -321,7 +321,7 @@ def stingray_setup(project_name: str, date: UTCDateTime):
             modeldict['S']['u'] = [] # modeldict['S']['u'] = Smod
             savemat(
                 "/Users/human/git/sigpig/sigpig/stingray/srInput/srModel_" + str(
-                    int(1000 * dz)) + "m_OSOS.mat",
+                    int(1000 * dz)) + "m_OSOS_0.75.mat",
                 {'srModel':modeldict})
 
     else:
