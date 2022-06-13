621fcc327a0302b188285b3120fc8ac21bd76903
Tyler Newton <tjnewton.uni@gmail.com>
2022-06-13 15:48:07 -0700

    delete notes from calculate_magnitude function

--- sigpig/core/data.py
+++ sigpig/core/data.py
@@ -2253,14 +2253,11 @@ def calculate_magnitude():
     Example:
         magnitudes = calculate_magnitude()
 
-        500 -> 40 magnitudes
-        20 -> error
-
     """
     # define the file paths containing the autopicked .mrkr file
     autopicked_file_path = "/Users/human/Dropbox/Programs/unet/autopicked_events_03_13_2018.mrkr"
     # define the desired number of events to get
-    n = 500
+    n = 100
     events = top_n_autopicked_events(autopicked_file_path, n)
 
     # make an empty list to store the streams for each event
